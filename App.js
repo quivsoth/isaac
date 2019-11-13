baf1ab2d83179307a4e6eea2a647063f6ddb4559
quivsoth <phillip.knezevich@gmail.com>
2019-11-13 17:42:12 -0500

    added reactn native game engine with sample

--- App.js
+++ App.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
-import { StyleSheet,View } from 'react-native';
+import { StyleSheet,View, TouchableOpacity, StatusBar, Alert } from 'react-native';
 import RemoteControl from './RemoteControl';
+import Constants from './Constants';
+import { GameEngine } from 'react-native-game-engine';
+import Matter from 'matter-js';
 
 export default class App extends Component {
 	constructor(props) {
-		super(props);    
+		super(props);
+		this.gameEngine = null;
+		this.entites = this.setupWorld();
+	}
+
+	setupWorld = () => {
+		let engine = Matter.engine.create({ enableSleeping: false });
+		let world = engine.world;
+		let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT /2, 50, 50);
+
+		Matter.World.add(world, [bird]);
 	}
 
 	render() {
 		return (
-      <View style={stylesMain.container}>
-        <RemoteControl></RemoteControl>
-			</View>
+      	<View style={styles.container}>
+			{/* <RemoteControl></RemoteControl> */}
+			<GameEngine
+				ref={(ref) => {this.gameEngine = ref;}}
+				style = {styles.container}
+				entities = { this.entities }
+				/>
+		</View>
 		);
 	}
 }
 
-const stylesMain = StyleSheet.create({
+const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		flexDirection: 'column',
