7cd5b091cad4972a28c80786b415c73300800853
QuivSoth <phillip.knezevich@gmail.com>
2019-11-12 19:26:44 -0500

    added functionality to show a panresponder

--- App.js
+++ App.js
@@ -1,38 +1,120 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+import { Animated, PanResponder, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+	constructor(props) {
+		super(props);
+
+		this.timer = null;
+		const heroCoords = null;
+		const heroPosition = new Animated.ValueXY();
+		const panResponder = PanResponder.create({
+			onStartShouldSetPanResponder: (evt, gestureState) => {
+				this.setState({ heroCoords: gestureState });
+				this.Move();
+			},
+			onMoveShouldSetPanResponder: (evt, gestureState) => true,
+			onPanResponderRelease: (event, gestureState) => {
+				this.clearTimer();
+			}
+		});
+
+		// Bindings
+		this.Move = this.Move.bind(this);
+		this.clearTimer = this.clearTimer.bind(this);
+
+		this.state = {
+			panResponder,
+			heroCoords,
+			heroPosition
+		};
+	}
+
+	// Functions =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	clearTimer() {
+		clearTimeout(this.timer);
+		console.log('Clearing Timer');
+	}
+
+	Move() {
+		if (this.isInCircle()) {
+			var heroX = (Number(JSON.stringify(this.state.heroPosition.x))) + Math.round(this.state.heroCoords.dx);
+			var heroY = (Number(JSON.stringify(this.state.heroPosition.y))) + Math.round(this.state.heroCoords.dy);
+			
+			//set the room boundaries
+			
+			Animated.spring(this.state.heroPosition, {toValue: {x: heroX, y: heroY}, }).start();
+		} else {
+			//console.log('Out of circle.');
+		}
+		this.timer = setTimeout(this.Move, 120);
+	}
+
+	isInCircle = () => {
+		// Pythagoras Theory - (x-x1)2 + (y-y1)2 = r2
+		var xCenter = 0;
+		var yCenter = 0;
+		var dx,
+			dy = 0;
+		if (this.state.heroCoords) {
+			dx = this.state.heroCoords.dx;
+			dy = this.state.heroCoords.dy;
+			// console.log('x :', dx);
+			// console.log('y :', dy);
+		}
+		var d = Math.sqrt(Math.pow(dx - xCenter, 2) + Math.pow(dy - yCenter, 2));
+		//console.log('direction : ' + d);
+		if (d < 100) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	// Render =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	render() {
+		let panHandlers = this.state.panResponder.panHandlers;
+		return (
+			<View style={styles.container}>
+				<View style={[ styles.top ]}>
+					<Animated.View style={[ styles.hero, this.state.heroPosition.getLayout() ]} />
+					{/*  <Text> {this.state.heroPosition} </Text> */}
+				</View>
+				<View style={[ styles.bottom ]}>
+					<View style={[ styles.dPad ]} {...panHandlers} />
+				</View>
+			</View>
+		);
+	}
 }
 
+// Styles =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center'
+	},
+	dPad: {
+		height: 200,
+		width: 200,
+		borderColor: 'black',
+		borderRadius: 100,
+		borderWidth: 100
+	},
+	hero: {
+		height: 80,
+		width: 80,
+		borderColor: 'blue',
+		borderRadius: 40,
+		backgroundColor: 'green'
+	},
+	top: {
+		flex: 3,
+		backgroundColor: 'gold'
+	},
+	bottom: {
+		flex: 1,
+		backgroundColor: 'powderblue'
+	}
 });
