c70211c48156c559d8ee40a3c886d32fc1e9f226
QuivSoth <phillip.knezevich@gmail.com>
2019-11-14 13:48:23 -0500

    matter.js tests

--- App.js
+++ App.js
@@ -1,38 +1,60 @@
 import React, { Component } from 'react';
-import { StyleSheet,View, TouchableOpacity, StatusBar, Alert } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, StatusBar, Alert } from 'react-native';
 import RemoteControl from './RemoteControl';
 import Constants from './Constants';
 import { GameEngine } from 'react-native-game-engine';
 import Matter from 'matter-js';
 
+
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.gameEngine = null;
-		this.entites = this.setupWorld();
+		var Engine = Matter.Engine,
+			World = Matter.World,
+			Bodies = Matter.Bodies
 	}
-
-	setupWorld = () => {
-		let engine = Matter.engine.create({ enableSleeping: false });
-		let world = engine.world;
-		let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT /2, 50, 50);
-
-		Matter.World.add(world, [bird]);
-	}
-
 	render() {
 		return (
-      	<View style={styles.container}>
-			{/* <RemoteControl></RemoteControl> */}
-			<GameEngine
-				ref={(ref) => {this.gameEngine = ref;}}
-				style = {styles.container}
-				entities = { this.entities }
-				/>
-		</View>
+			<View>
+				<Text>Why helsslo</Text>
+			</View>
 		);
 	}
 }
+// export default class App extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.gameEngine = null;
+// 		this.entites = this.setupWorld();
+// 	}
+
+// 	setupWorld = () => {
+// 		let engine = Matter.engine.create({ enableSleeping: false });
+// 		let world = engine.world;
+// 		let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT /2, 50, 50);
+
+// 		Matter.World.add(world, [bird]);
+
+// 		return {
+// 			physics: { engine: engine, world: world },
+// 			bird: { body: bird, size: [50,50], color: 'red', rederer: Bird }
+// 		}
+// 	}
+
+// 	render() {
+// 		return (
+//       	<View style={styles.container}>
+// 			{/* <RemoteControl></RemoteControl> */}
+// 			<GameEngine
+// 				ref={(ref) => {this.gameEngine = ref;}}
+// 				style = {styles.container}
+// 				entities = { this.entities }
+// 				/>
+// 		</View>
+// 		);
+// 	}
+// }
 
 const styles = StyleSheet.create({
 	container: {
